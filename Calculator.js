76f93473f913e7ab476a3410259b4a4f3ebcffd6
saivarun6385 <84833239+saivarun6385@users.noreply.github.com>
2023-11-07 13:21:03 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,24 @@
+let num1=2
+let num2=8
+document.getElementById("n1").textContent=num1
+document.getElementById("n2").textContent=num2
+
+
+
+let s=document.getElementById("sum")
+document.getElementById("add").onclick=function(){
+    let res=num1+num2
+    s.textContent="Sum:"+ res
+}
+document.getElementById("sub").onclick=function(){
+    let res=num1-num2
+    s.textContent="sum:"+res
+}
+document.getElementById("div").onclick=function(){
+    let res=num1/num2
+    s.textContent="sum:"+res
+}
+document.getElementById("mul").onclick=function(){
+    let res=num1*num2
+    s.textContent="sum:"+res
+}
